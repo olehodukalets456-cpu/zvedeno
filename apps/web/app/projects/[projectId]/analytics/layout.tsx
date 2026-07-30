f02101aee39642241cfe537340f698facf2004fe
Oleh <oleh.odukalets456@gmail.com>
2026-07-30 17:49:54 +0300

    Load analytics scroll alignment styles

--- apps/web/app/projects/[projectId]/analytics/layout.tsx
+++ apps/web/app/projects/[projectId]/analytics/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./analytics.module.css";
+import "./analytics-overrides.css";
 
 export default function AnalyticsLayout({ children }: { children: ReactNode }) {
   return children;
