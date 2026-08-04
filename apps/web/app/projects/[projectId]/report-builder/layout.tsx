2f912f4352c37630e81469dcfbd08ddbc212ee52
Oleh <oleh.odukalets456@gmail.com>
2026-08-04 20:43:37 +0300

    Load report builder styles

---
+++ apps/web/app/projects/[projectId]/report-builder/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./report-builder.css";
+
+export default function ReportBuilderLayout({ children }: { children: ReactNode }) {
+  return children;
+}
